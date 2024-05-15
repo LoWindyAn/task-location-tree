a5e0114ea918d96e51d043fdd039635e0e27caa0
Sang Nguyen <nsancukty100@gmail.com>
2024-05-15 20:16:21 +0700

    Changes

--- src/components/LocationTree.jsx
+++ src/components/LocationTree.jsx
@@ -1,24 +1,52 @@
 import { IoCloseCircleOutline, IoSearch } from "react-icons/io5"
 import Location from "./Location"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import axios from "axios"
 import { MdOutlineClose } from "react-icons/md"
 
 const LocationTree = () => {
     const [data, setData] = useState('')
     const [allData, setAllData] = useState('')
-    const [isVisible, setIsVisible] = useState(4)
+    const [visible, setVisible] = useState(16)
+    const [isLoadmore, setIsLoadMore] = useState(false)
+    const containerRef = useRef()
+
     useEffect(() => {
+
         const fetchData = async () => {
             const res = await (await axios.get("https://66433cbc3c01a059ea221251.mockapi.io/api/locationtree/location"))
             setAllData(res.data)
-            setData(res.data.slice(0, 2))
+            setData(res.data.slice(0, 16))
         }
+
         fetchData()
+
     }, [])
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            if (containerRef.current) {
+                const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+                if (visible <= allData.length + 1 && scrollTop + clientHeight >= scrollHeight - 5) {
+                    setIsLoadMore(true)
+                    LoadMore();
+                }
+            }
+        };
+
+        containerRef.current.addEventListener('scroll', handleScroll);
+
+        return () => {
+            containerRef.current.removeEventListener('scroll', handleScroll);
+        };
+
+    }, [data]);
+
     const LoadMore = () => {
-        setData(allData.slice(0, isVisible))
-        setIsVisible(isVisible + 2)
+        setData(allData.slice(0, visible))
+        setVisible(visible + 2)
+        setIsLoadMore(false)
     }
 
     return (
@@ -28,15 +56,13 @@ const LocationTree = () => {
                 <input type="text" className="w-full border pl-6 h-8" />
                 <MdOutlineClose className="absolute right-5 font-semibold text-[16px] text-[#c6c4c4] hover:text-[#b3b3b3] cursor-pointer" />
             </div>
-            <div className="h-[500px] p-4 overflow-y-scroll">
+            <div ref={containerRef} className="h-[400px] p-4 overflow-y-scroll">
                 {
                     data ? data.map((item, index) => (
                         <Location index={index} item={item} key={index} />
-                    )) : <p>Loading ... </p>
+                    )) : <p className="italic opacity-50">Loading ... </p>
                 }
-
-                {isVisible <= allData.length ? <p onClick={LoadMore} className="text-[#7c7c7c] italic cursor-pointer hover:text-blue-700">Loadmore ...</p> : ""}
-
+                {isLoadmore ? <div className="italic opacity-50">...</div> : ""}
             </div>
         </div>
     )
